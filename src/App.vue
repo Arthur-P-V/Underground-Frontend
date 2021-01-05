17c60b72af26937ee48292b1bde9a71965cb1a8e
Arthur PV <apetersonveatch@gmail.com>
2021-01-05 16:26:06 -0600

    Begins applying theme

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,63 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <h1>Underground</h1>
+    <div class="header-bottom">
+      <div class="container-fluid">
+        <button
+          type="button"
+          class="navbar-toggle collapsed"
+          data-toggle="collapse"
+          data-target="#main-nav-container"
+          aria-expanded="false"
+        >
+          <span class="toggle-text">Menu</span>
+          <span class="toggle-wrapper">
+            <span class="sr-only">Toggle Menu</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </span>
+        </button>
+        <div class="collapse navbar-collapse" id="main-nav-container">
+          <ul class="nav navbar-nav">
+            <li class="dropdown active">
+              <a href="/" role="button" aria-expanded="false">
+                Home
+              </a>
+            </li>
+            <li>
+              <a v-if="!isLoggedIn()" href="/signup" role="button" aria-expanded="false">
+                Sign Up
+              </a>
+            </li>
+            <li>
+              <a v-if="!isLoggedIn()" href="/login" role="button" aria-expanded="false">
+                Log In
+              </a>
+            </li>
+            <li>
+              <a v-if="isLoggedIn()" href="/posts/new" role="button" aria-expanded="false">
+                Create Post
+              </a>
+            </li>
+            <li>
+              <a v-if="isLoggedIn()" v-bind:to="`/users/${this.getUser}`" role="button" aria-expanded="false">
+                Your Account
+              </a>
+            </li>
+            <li>
+              <a v-if="isLoggedIn()" href="/logout" role="button" aria-expanded="false">
+                Log Out
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+      <!-- End .container-fluid -->
+    </div>
+    <!-- End .header-bottom -->
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link>
       |
       <router-link to="/about">About</router-link>
@@ -14,33 +71,54 @@
       <router-link v-if="isLoggedIn()" to="/posts/new">Create Post</router-link>
       |
       <router-link v-if="isLoggedIn()" :to="`/users/${getUser()}`">Your Account</router-link>
-    </div>
+    </div> -->
     <router-view />
+    <footer class="footer">
+      <div class="footer-bottom">
+        <div class="container-fluid">
+          <div class="footer-left">
+            <div class="social-icons">
+              <label>Find Us:</label>
+              <a href="#" class="social-icon" title="Facebook"><i class="fa fa-facebook"></i></a>
+              <a href="#" class="social-icon" title="Twitter"><i class="fa fa-twitter"></i></a>
+              <a href="#" class="social-icon" title="Github"><i class="fa fa-github"></i></a>
+              <a href="#" class="social-icon" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+              <a href="#" class="social-icon" title="Tumblr"><i class="fa fa-tumblr"></i></a>
+              <a href="#" class="social-icon" title="Flickr"><i class="fa fa-flickr"></i></a>
+              <a href="#" class="social-icon" title="Snapchat"><i class="fa fa-snapchat"></i></a>
+              <a href="#" class="social-icon" title="Vine"><i class="fa fa-vine"></i></a>
+            </div>
+            <!-- End .social-icons -->
+          </div>
+          <!-- End .footer-left -->
+
+          <div class="footer-right">
+            <ul class="footer-menu">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Features</a></li>
+              <li><a href="#">FaQS</a></li>
+              <li><a href="#">Support</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+            <p class="copyright">
+              <strong>Bold</strong>
+              multipurpose template - All rights reserved - 2016 &copy; Made by
+              <a href="http://eonythemes.com" target="_blank" title="eonythemes">eonythemes</a>
+              .
+            </p>
+          </div>
+          <!-- End .footer-right -->
+        </div>
+        <!-- End .container-fluid -->
+      </div>
+      <!-- End .footer-bottom -->
+    </footer>
+    <!-- End .footer -->
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 <script>
 export default {
   methods: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,127 @@
 <template>
   <div class="home">
-    <h1>Underground</h1>
     <!-- <h2>{{ category }}</h2> -->
-    <button v-on:click="indexCategory">All</button>
+    <div class="main">
+      <div
+        class="page-header largest parallax custom text-center"
+        v-bind:style="`background-image:url(assets/images/page-header-bg.jpg)`"
+        data-0="background-position:50% 50%;"
+        data-top-bottom="background-position:50% 100%"
+      >
+        <div class="container-fluid">
+          <h1 data-300-top="opacity:1;transform:scale(1);" data-top-bottom="opacity:0.4;transform:scale(1.9);">Blog</h1>
+          <ol class="breadcrumb" data-300-top="opacity:1;" data-top-bottom="opacity:0.4;">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#">Pages</a></li>
+            <li class="active">Blog</li>
+          </ol>
+        </div>
+        <!-- End .container-fluid -->
+      </div>
+      <!-- End .page-header -->
+
+      <div class="container-fluid">
+        <ul id="portfolio-filter">
+          <li class="active"><a v-on:click="indexCategory">All</a></li>
+          <li><a v-on:click="gamesCategory">Games</a></li>
+          <li><a v-on:click="moviesCategory">Movies</a></li>
+          <li><a v-on:click="musicCategory">Music</a></li>
+        </ul>
+      </div>
+      <!-- <button v-on:click="indexCategory">All</button>
     <button v-on:click="gamesCategory">Games</button>
     <button v-on:click="musicCategory">Music</button>
-    <button v-on:click="moviesCategory">Movies</button>
-    <div v-for="post in filterBy(posts, `${category}`, 'category')" :key="post.id">
-      <router-link :to="`/posts/${post.id}`">
-        <h1>{{ post.title }}</h1>
-        <router-link :to="`/users/${post.user.id}`">
-          <h3>{{ post.user.name }}</h3>
-        </router-link>
-        <h4>{{ post.category }}</h4>
-        <h5>{{ post.score }}</h5>
-        <p>{{ post.content }}</p>
-      </router-link>
+    <button v-on:click="moviesCategory">Movies</button> -->
+      <div class="container-fluid">
+        <div class="blog-row">
+          <div id="blog-item-container" class="max-col-2" data-layoutmode="fitRows">
+            <div v-for="post in filterBy(posts, `${category}`, 'category')" :key="post.id">
+              <article class="entry entry-grid">
+                <div class="entry-media">
+                  <figure>
+                    <a href="single.html"><img v-bind:src="post.image_url" alt="Post image" /></a>
+                  </figure>
+                </div>
+                <!-- End .entry-media -->
+
+                <!-- <div class="entry-icon">
+                <i class="fa fa-file-image-o"></i>
+              </div> -->
+                <!-- End .entry-icon -->
+
+                <div class="entry-content-container">
+                  <!-- <div class="entry-meta">
+                  <div>
+                    <i class="fa fa-calendar"></i>
+                    17 Aug 2016
+                  </div>
+                  <div>
+                    <a href="#">
+                      <i class="fa fa-comments"></i>
+                      8 Comment(s)
+                    </a>
+                  </div>
+                </div> -->
+                  <!-- End .entry-meta -->
+
+                  <div class="entry-content-wrapper">
+                    <h2 class="entry-title">
+                      {{ post.title }}
+                    </h2>
+                    <div class="entry-content">
+                      <p>
+                        {{ post.content }}
+                      </p>
+                      <a v-bind:href="`/posts/${post.id}`" class="readmore">
+                        Read more
+                        <i class="fa fa-angle-right"></i>
+                      </a>
+                    </div>
+                    <!-- End .entry-content -->
+                  </div>
+                  <!-- End .entry-content-wrapper -->
+                </div>
+                <!-- End .entry-content-container -->
+              </article>
+              <!-- <router-link :to="`/posts/${post.id}`">
+              <h2>{{ post.title }}</h2>
+              <router-link :to="`/users/${post.user.id}`">
+                <h3>{{ post.user.name }}</h3>
+              </router-link>
+              <h4>{{ post.category }}</h4>
+              <h5>{{ post.score }}</h5>
+              <p>{{ post.content }}</p>
+            </router-link> -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="pagination-wrapper">
+        <nav class="pagination-container">
+          <label>Showing: 1-4 of 16</label>
+          <ul class="pagination">
+            <li class="active"><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li>
+              <a href="#" aria-label="Next">
+                <span aria-hidden="true"><i class="fa fa-angle-right"></i></span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <!-- End .pagination-wrapper -->
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+h1 {
+  text-align: center;
+}
+</style>
 
 <script>
 import axios from "axios";
@@ -32,7 +133,7 @@ export default {
     return {
       posts: [],
       // eslint-disable-next-line prettier/prettier
-      category: '',
+      category: "",
       check: "",
     };
   },
@@ -48,19 +149,19 @@ export default {
     },
     gamesCategory: function() {
       // eslint-disable-next-line prettier/prettier
-      this.category = 'Games';
+      this.category = "Games";
     },
     indexCategory: function() {
       // eslint-disable-next-line prettier/prettier
-      this.category = ''
+      this.category = "";
     },
     musicCategory: function() {
       // eslint-disable-next-line prettier/prettier
-      this.category = 'Music'
+      this.category = "Music";
     },
     moviesCategory: function() {
       // eslint-disable-next-line prettier/prettier
-      this.category = 'Movies'
+      this.category = "Movies";
     },
   },
 };
